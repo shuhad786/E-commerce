b256384640fffb413d5f508af61dcb3bba13bb25
shuhad786 <shuhad.loofer@gmail.com>
2023-06-20 20:13:48 +0200

    Update Modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -4,7 +4,6 @@ function Modal({ selectedItem, closeModal }) {
   if (!selectedItem) {
     return null; // Don't render the modal if no item is selected
   }
-
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-transparent backdrop-blur">
       <div className="bg-neutral-100 w-11/12 p-4 rounded-lg shadow-xl shadow-neutral-400 flex relative">
@@ -17,7 +16,6 @@ function Modal({ selectedItem, closeModal }) {
           <p className="text-md font-bold mt-4 bg-white p-1 rounded-lg shadow-inner shadow-slate-400 text-sm font-semibold text-black">
             <span className="text-sm font-semibold text-slate-500">{selectedItem.description}</span>
           </p>
-          {/* Display other item details as needed */}
         </div>
         <button
           className="text-emerald-400 bg-tranparent text-3xl mr-3 mt-1 absolute top-2 right-2 "
